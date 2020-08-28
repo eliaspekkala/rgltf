df28a393e22a2e4c59eb1922aa82f0956feb1c41
Elias Pekkala <eliaspekkala98@gmail.com>
2020-08-28 13:26:52 +0200

    Stop generating bindings on every build

--- build.rs
+++ build.rs
@@ -1,15 +1,15 @@
 fn main() {
-    bindgen::builder()
-        .header("vendor/cgltf/cgltf.h")
-        .header("vendor/cgltf/cgltf_write.h")
-        .whitelist_type("cgltf.*")
-        .whitelist_function("cgltf.*")
-        .whitelist_var("cgltf.*")
-        .layout_tests(false)
-        .generate()
-        .expect("Could not generate bindings")
-        .write_to_file("src/ffi.rs")
-        .expect("Could not write bindings to file");
+    // bindgen::builder()
+    //     .header("vendor/cgltf/cgltf.h")
+    //     .header("vendor/cgltf/cgltf_write.h")
+    //     .whitelist_type("cgltf.*")
+    //     .whitelist_function("cgltf.*")
+    //     .whitelist_var("cgltf.*")
+    //     .layout_tests(false)
+    //     .generate()
+    //     .expect("Could not generate bindings")
+    //     .write_to_file("src/ffi.rs")
+    //     .expect("Could not write bindings to file");
 
     cc::Build::new()
         .file("cgltf.c")
